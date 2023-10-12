7a3d0b35314ca734d24f21abfca672a8e91164bd
marcin10lw <marcin10lw@wp.pl>
2023-10-12 02:52:54 +0200

    adjusted backdrop winner colo

--- src/components/ResultBackdrop.tsx
+++ src/components/ResultBackdrop.tsx
@@ -9,8 +9,8 @@ type ResultBackdropProps = {
 const ResultBackdrop = ({ winner, playAgain }: ResultBackdropProps) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-[hsl(0deg_0%_0%_/_50%)] px-5">
-      <div className="animate-pop aspect-video w-full max-w-md rounded-2xl bg-white p-8 text-center shadow-2xl">
-        <span className="block text-2xl tracking-wider lg:text-3xl">Winner:</span>
+      <div className="animate-pop aspect-video w-full max-w-md rounded-2xl bg-slate-200 p-8 text-center shadow-2xl">
+        <span className="block text-2xl tracking-wider text-slate-950 lg:text-3xl">Winner:</span>
         <span className="mt-2 block text-4xl capitalize tracking-wider text-slate-700 lg:text-5xl">
           {winner}
         </span>
